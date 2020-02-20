d9f249386d68dcea3436446cfa29d14567d99a2c
Zephyr Zambrano <43213451+zephyrzambrano@users.noreply.github.com>
2020-02-19 22:36:58 -0500

    Added updated file

--- hw_05.py
+++ hw_05.py
@@ -8,41 +8,41 @@ Zephyr Zambrano
 import unittest
 
 
-def classify_triangle(a, b, c):
+def classify_triangle(side_a, side_b, side_c):
     """
-    This function returns a string with the type of triangle from three  values
+    This function returns a string with the type of triangle from three values
     corresponding to the lengths of the three sides of the Triangle.
-    
+
     return:
         If all three sides are equal, return 'Equilateral'
         If exactly one pair of sides are equal, return 'Isoceles'
         If no pair of  sides are equal, return 'Scalene'
         If not a valid triangle, then return 'NotATriangle'
         If the sum of any two sides equals the square of the third side, then return 'Right'
-    """    
-    if a <=0 or b <= 0 or c <= 0: # side can't have length of 0 or negative length
+    """
+    if side_a <= 0 or side_b <= 0 or side_c <= 0: # side can't have length of 0 or negative length
         return "Not A Triangle"
-    if a == b and a == c and b == c:
+    elif side_a == side_b and side_a == side_c and side_b == side_c:
         return "Equilateral"
-    elif a * a + b * b == c * c:
+    elif side_a * side_a + side_b * side_b == side_c * side_c:
         return "Right"
-    elif a != b and a != c and b != c:
-        return "Scalene"    
-    elif a == b or a == c or b == c:
+    elif side_a != side_b and side_a != side_c and side_b != side_c:
+        return "Scalene"
+    elif side_a == side_b or side_a == side_c or side_b == side_c:
         return "Isoceles"
     else:
         return "Not A Triangle"
-        
-        
-def run_classify_triangle(a, b, c):
+
+
+def run_classify_triangle(side_a, side_b, side_c):
     """ Runs the classify_triangle() method and prints the result to the user. """
-    answer = classify_triangle(a, b, c)
-    print(f"A triangle with sides of length {a}, {b}, and {c} is: {answer}")
+    answer = classify_triangle(side_a, side_b, side_c)
+    print(f"A triangle with sides of length {side_a}, {side_b}, and {side_c} is: {answer}")
 
 
 class TestTriangles(unittest.TestCase):
     """ Tests the triangle classifications. """
-    
+
     def test_classify_triangle(self):
         """ Tests the classify_triangle() method. """
         # the four different types of triangles
@@ -50,11 +50,11 @@ class TestTriangles(unittest.TestCase):
         self.assertEqual(classify_triangle(1, 1, 1), "Equilateral")
         self.assertEqual(classify_triangle(3, 3, 7), "Isoceles")
         self.assertEqual(classify_triangle(3, 4, 6), "Scalene")
-        
+
         # Not A Triangle
         self.assertEqual(classify_triangle(4, 5, 0), "Not A Triangle") # side of length 0
         self.assertEqual(classify_triangle(4, 1, -3), "Not A Triangle") # side with negative length
-        self.assertEqual(classify_triangle(0, 1, -3), "Not A Triangle") # side of length 0 and side with negative length
+        self.assertEqual(classify_triangle(0, 1, -3), "Not A Triangle") # 0 and negative side
 
 
 if __name__ == "__main__":
@@ -63,5 +63,5 @@ if __name__ == "__main__":
     run_classify_triangle(3, 4, 5)
     run_classify_triangle(1, 1, 4)
     run_classify_triangle(0, 0, 0)
-    
+
     unittest.main(exit=False)
